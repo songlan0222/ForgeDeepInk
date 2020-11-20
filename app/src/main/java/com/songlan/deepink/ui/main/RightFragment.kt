95b987489360e268bc1dedaff9b45810ce69e517
songlan0222 <651749248@qq.com>
2020-11-20 15:50:18 +0800

    创建MyApplication类，用于方便获取全局context对象； 因为此处为titleBar添加图标时，会用上context对象；

--- app/src/main/java/com/songlan/deepink/ui/main/RightFragment.kt
+++ app/src/main/java/com/songlan/deepink/ui/main/RightFragment.kt
@@ -1,12 +1,17 @@
 package com.songlan.deepink.ui.main
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.hjq.bar.OnTitleBarListener
+import com.songlan.deepink.MyApplication
 import com.songlan.deepink.R
 import kotlinx.android.synthetic.main.fragment_main_left.*
+import kotlinx.android.synthetic.main.fragment_main_right.*
 
 class RightFragment : Fragment() {
 
@@ -15,13 +20,30 @@ class RightFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main_left, container, false)
+        return inflater.inflate(R.layout.fragment_main_right, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 配置顶部工具栏
-        main_left_toolbar.inflateMenu(R.menu.main_left_toolbar_menu)
+        // 配置顶部TitleBar
+        main_right_titleBar.leftIcon =
+            ContextCompat.getDrawable(MyApplication.context, R.drawable.ic_main_right_experiencer)
+        main_right_titleBar.rightIcon =
+            ContextCompat.getDrawable(MyApplication.context, R.drawable.ic_main_right_subscriber)
+
+        // 添加点击事件
+        main_right_titleBar.setOnTitleBarListener(object : OnTitleBarListener {
+            override fun onLeftClick(v: View?) {
+            }
+
+            // 此处用不上中间的点击事件
+            override fun onTitleClick(v: View?) {
+            }
+
+            override fun onRightClick(v: View?) {
+            }
+
+        })
     }
 }

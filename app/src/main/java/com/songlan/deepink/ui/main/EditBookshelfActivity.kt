85f508420107c0aab170a80818d23c48cbf461c4
songlan0222 <651749248@qq.com>
2020-12-09 16:29:46 +0800

    修改圆角矩形的背景颜色

--- app/src/main/java/com/songlan/deepink/ui/main/EditBookshelfActivity.kt
+++ app/src/main/java/com/songlan/deepink/ui/main/EditBookshelfActivity.kt
@@ -1,10 +1,13 @@
 package com.songlan.deepink.ui.main
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.songlan.deepink.R
+import kotlinx.android.synthetic.main.activity_edit_bookshelf.*
 
 class EditBookshelfActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_bookshelf)

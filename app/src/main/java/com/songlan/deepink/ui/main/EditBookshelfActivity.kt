c0fea79651a974b35f44c65d240da46e7eeac2f2
songlan0222 <651749248@qq.com>
2020-12-10 09:36:19 +0800

    添加editText的Focus，并弹出软键盘

--- app/src/main/java/com/songlan/deepink/ui/main/EditBookshelfActivity.kt
+++ app/src/main/java/com/songlan/deepink/ui/main/EditBookshelfActivity.kt
@@ -1,9 +1,12 @@
 package com.songlan.deepink.ui.main
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
 import com.songlan.deepink.R
 import kotlinx.android.synthetic.main.activity_edit_bookshelf.*
 
@@ -18,6 +21,14 @@ class EditBookshelfActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
         supportActionBar?.title = null
         textView_title.text = getString(R.string.edit_bookshelf_info)
+
+        // editText 获取焦点
+        editText_bookshelfName.isFocusable = true
+        editText_bookshelfName.isFocusableInTouchMode = true
+        editText_bookshelfName.requestFocus()
+        // 弹出软键盘
+        val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        manager.showSoftInput(editText_bookshelfName, 0)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -26,6 +37,6 @@ class EditBookshelfActivity : AppCompatActivity() {
                 finish()
             }
         }
-        return true
+        return super.onOptionsItemSelected(item)
     }
 }

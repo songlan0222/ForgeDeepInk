ba0076e2e1c95bb0a7c49be5f21624c4841939b3
songlan0222 <651749248@qq.com>
2020-12-10 09:30:33 +0800

    添加EditBookshelfActivity中的toolbar返回按钮和居中的标题

--- app/src/main/java/com/songlan/deepink/ui/main/EditBookshelfActivity.kt
+++ app/src/main/java/com/songlan/deepink/ui/main/EditBookshelfActivity.kt
@@ -3,6 +3,7 @@ package com.songlan.deepink.ui.main
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.songlan.deepink.R
 import kotlinx.android.synthetic.main.activity_edit_bookshelf.*
 
@@ -11,5 +12,20 @@ class EditBookshelfActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_bookshelf)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        supportActionBar?.title = null
+        textView_title.text = getString(R.string.edit_bookshelf_info)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+        return true
     }
 }

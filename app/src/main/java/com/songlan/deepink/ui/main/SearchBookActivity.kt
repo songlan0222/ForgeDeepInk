c923a4674d98a6524f64dc10894631922260cdf6
songlan0222 <651749248@qq.com>
2020-12-11 11:38:01 +0800

    完善搜索栏界面样式

--- app/src/main/java/com/songlan/deepink/ui/main/SearchBookActivity.kt
+++ app/src/main/java/com/songlan/deepink/ui/main/SearchBookActivity.kt
@@ -2,6 +2,7 @@ package com.songlan.deepink.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import com.songlan.deepink.R
 import kotlinx.android.synthetic.main.activity_search_book.*
@@ -12,8 +13,26 @@ class SearchBookActivity : AppCompatActivity() {
         setContentView(R.layout.activity_search_book)
 
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        supportActionBar?.title = null
 
-        linearLayout_searchBook
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_book_search_src, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+            R.id.bookSrc -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

18e94284cb1f0c91883ab16a5eb9de6d380a75fc
songlan0222 <651749248@qq.com>
2020-12-15 09:43:14 +0800

    修改： 1. SearchBookActivity中，取消ViewPager展示底部界面内容，改为使用FrameLayout动态展示Fragment内容，因为用不上左右滑动； 2. 使用ViewModel加载所需要用上的两个Fragment，以保证取消搜索后，搜索历史还存在

--- app/src/main/java/com/songlan/deepink/ui/main/SearchBookActivity.kt
+++ app/src/main/java/com/songlan/deepink/ui/main/SearchBookActivity.kt
@@ -10,6 +10,7 @@ import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
 import com.songlan.deepink.R
 import kotlinx.android.synthetic.main.activity_search_book.*
@@ -17,6 +18,9 @@ import kotlinx.android.synthetic.main.activity_search_book.*
 class SearchBookActivity : AppCompatActivity() {
 
     private val fragmentMap = mutableMapOf<Int, Fragment>()
+    private val vm by lazy {
+        ViewModelProvider(this).get(SearchBookActivityVM::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,18 +35,19 @@ class SearchBookActivity : AppCompatActivity() {
         editText_searchBookName.isFocusable = true
         editText_searchBookName.isFocusableInTouchMode = true
         editText_searchBookName.requestFocus()
-//        绑定获取焦点
-//        editText_searchBookName.viewTreeObserver.addOnGlobalLayoutListener {
-//
-//        }
         // 弹出软键盘
         val manager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         manager.showSoftInput(editText_searchBookName, 0)
 
-        // 配置ViewPager
-        viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
-        viewPager.currentItem = SearchBookActivityVM.DEFAULT_FRAGMENT
+        // 动态配置FrameLayout
+        replaceFragment(vm.searchBookHistoryFragment)
+    }
 
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout_search, fragment)
+        transaction.commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -60,24 +65,5 @@ class SearchBookActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    private inner class ViewPagerAdapter(fragmentManager: FragmentManager) :
-        FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
-        override fun getCount() = 2
-
-        override fun getItem(position: Int) =
-            when (position) {
-                0 -> fragmentMap[0] ?: SearchBookHistoryFragment()
-                else -> fragmentMap[1] ?: SearchBookResultFragment()
-            }
-
-        override fun instantiateItem(container: ViewGroup, position: Int): Any {
-            val fragment = super.instantiateItem(container, position) as Fragment
-            fragmentMap[position] = fragment
-            return fragment
-        }
-
-
     }
 }
--- app/src/main/java/com/songlan/deepink/ui/main/SearchBookActivityVM.kt
+++ app/src/main/java/com/songlan/deepink/ui/main/SearchBookActivityVM.kt
@@ -1,12 +1,20 @@
 package com.songlan.deepink.ui.main
 
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 
-class SearchBookActivityVM: ViewModel() {
+class SearchBookActivityVM : ViewModel() {
 
-    companion object{
+    companion object {
         const val DEFAULT_FRAGMENT = 0
         const val SEARCH_HISTORY_FRAGMENT = 0
         const val SEARCH_RESULT_FRAGMENT = 1
+    }
+
+    val searchBookHistoryFragment: Fragment by lazy {
+        SearchBookHistoryFragment()
+    }
+    val searchBookResultFragment: SearchBookResultFragment by lazy {
+        SearchBookResultFragment()
     }
 }

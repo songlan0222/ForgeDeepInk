2348898bd80b2358d672689ad6c493820d958179
songlan0222 <651749248@qq.com>
2020-12-15 10:57:48 +0800

    添加：热书推荐部分

--- app/src/main/java/com/songlan/deepink/ui/main/SearchBookActivityVM.kt
+++ app/src/main/java/com/songlan/deepink/ui/main/SearchBookActivityVM.kt
@@ -27,4 +27,17 @@ class SearchBookActivityVM : ViewModel() {
         it.add("全球高武")
         it
     }
+
+    val SearchBookActivityHotBookList by lazy{
+        val it = mutableListOf<String>()
+        it.add("超神机械师")
+        it.add("吞噬星空")
+        it.add("大奉打更人")
+        it.add("剑来")
+        it.add("大王饶命")
+        it.add("我师兄实在太稳了")
+        it.add("小阁老")
+        it.add("精灵掌门人")
+        it
+    }
 }
--- app/src/main/java/com/songlan/deepink/ui/main/SearchBookHistoryFragment.kt
+++ app/src/main/java/com/songlan/deepink/ui/main/SearchBookHistoryFragment.kt
@@ -1,20 +1,27 @@
 package com.songlan.deepink.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.songlan.deepink.R
 import kotlinx.android.synthetic.main.fragment_search_book_history.*
+import kotlinx.android.synthetic.main.item_search_book_hot.*
 
 class SearchBookHistoryFragment : Fragment() {
 
     private lateinit var searchBookActivity: SearchBookActivity
+
+    enum class DataType {
+        HOT_BOOK, HISTORY
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,35 +36,77 @@ class SearchBookHistoryFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        recyclerView_hotBook.adapter = MyRecyclerViewAdapter(
+            searchBookActivity.vm.SearchBookActivityHotBookList,
+            DataType.HOT_BOOK
+        )
+        recyclerView_hotBook.layoutManager = LinearLayoutManager(searchBookActivity)
+
         recyclerView_searchHistory.adapter =
-            MyRecyclerViewAdapter(searchBookActivity.vm.searchBookHistoryList)
+            MyRecyclerViewAdapter(searchBookActivity.vm.searchBookHistoryList, DataType.HISTORY)
         recyclerView_searchHistory.layoutManager = LinearLayoutManager(searchBookActivity)
 
     }
 
-    inner class MyRecyclerViewAdapter(private val historyList: List<String>) :
-        RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder>() {
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class MyRecyclerViewAdapter(
+        private val dataList: List<String>,
+        private val dataType: DataType
+    ) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        inner class HistoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             val history: TextView = view.findViewById(R.id.textView_history)
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_search_book_history, parent, false)
-            val viewHolder = ViewHolder(view)
-            viewHolder.itemView.setOnClickListener {
-                val position = viewHolder.adapterPosition
-                val history = historyList[position]
+        inner class HotBookViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val hotLogo: ImageView = view.findViewById(R.id.imageView_hotLogo)
+            val bookName: TextView = view.findViewById(R.id.textView_hotBookName)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+            return if (dataType == DataType.HISTORY) {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_search_book_history, parent, false)
+                val viewHolder = HistoryViewHolder(view)
+                viewHolder.itemView.setOnClickListener {
+                    val position = viewHolder.adapterPosition
+                    val history = dataList[position]
+                }
+                viewHolder
+            } else {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_search_book_hot, parent, false)
+                val viewHolder = HotBookViewHolder(view)
+                viewHolder.itemView.setOnClickListener {
+                    val position = viewHolder.adapterPosition
+                    val bookName = dataList[position]
+                }
+                viewHolder
             }
-            return viewHolder
+
         }
 
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val history = historyList[position]
-            holder.history.text = history
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            if (dataType == DataType.HISTORY) {
+                val history = dataList[position]
+                holder as HistoryViewHolder
+                holder.history.text = history
+            } else {
+                val bookName = dataList[position]
+                holder as HotBookViewHolder
+                holder.bookName.text = bookName
+                if (position < 3) {
+                    holder.hotLogo.setImageResource(R.drawable.ic_search_book_hot)
+                    holder.hotLogo.visibility = View.VISIBLE
+                } else {
+                    holder.hotLogo.visibility = View.GONE
+                }
+            }
+
         }
 
-        override fun getItemCount(): Int = if (historyList.size < 20) historyList.size else 20
+        override fun getItemCount(): Int = if (dataList.size < 20) dataList.size else 20
     }
 
 }

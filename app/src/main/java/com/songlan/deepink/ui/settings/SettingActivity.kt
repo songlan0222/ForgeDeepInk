5870ab1b6b0b054bedcc3e96f1ab4ef9d6251b71
songlan0222 <651749248@qq.com>
2020-12-22 15:03:20 +0800

    添加： 1. 绑定选项点击事件和开关的切换 2. 为开关添加状态监听器

--- app/src/main/java/com/songlan/deepink/ui/settings/SettingActivity.kt
+++ app/src/main/java/com/songlan/deepink/ui/settings/SettingActivity.kt
@@ -3,10 +3,11 @@ package com.songlan.deepink.ui.settings
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import com.songlan.deepink.R
 import kotlinx.android.synthetic.main.activity_setting.*
 
-class SettingActivity : AppCompatActivity() {
+class SettingActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
@@ -17,14 +18,84 @@ class SettingActivity : AppCompatActivity() {
             it.setHomeAsUpIndicator(R.drawable.ic_back)
             it.title = null
         }
+
+        // 为选项配置点击事件
+        testVersion.setOnClickListener(this)
+        accountCenter.setOnClickListener(this)
+        checkUpdate.setOnClickListener(this)
+
+        signNotify.setOnClickListener(this)
+        fontSize.setOnClickListener(this)
+        followSystemTheme.setOnClickListener(this)
+
+        chasingMode.setOnClickListener(this)
+
+        shootScreen.setOnClickListener(this)
+
+        useSpace.setOnClickListener(this)
+        userGuide.setOnClickListener(this)
+
+        userAgreement.setOnClickListener(this)
+        openSourceAgreement.setOnClickListener(this)
+
+        // 为几个开关添加监听事件
+        // 签到提醒
+        signNotifySwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+
+        }
+        // 中等字号
+        middleFontSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+
+        }
+        // 系统主题
+        followSystemThemeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+
+        }
+        // 追更模式
+        chasingModeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             android.R.id.home -> {
                 finish()
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.testVersion -> {
+
+            }
+            R.id.checkUpdate -> {
+            }
+            R.id.signNotify -> {
+                signNotifySwitch.isChecked = !signNotifySwitch.isChecked
+            }
+            R.id.fontSize -> {
+                middleFontSwitch.isChecked = !middleFontSwitch.isChecked
+            }
+            R.id.followSystemTheme -> {
+                followSystemThemeSwitch.isChecked = !followSystemThemeSwitch.isChecked
+            }
+            R.id.chasingMode -> {
+                chasingModeSwitch.isChecked = !chasingModeSwitch.isChecked
+            }
+            R.id.shootScreen -> {
+            }
+            R.id.useSpace -> {
+            }
+            R.id.userGuide -> {
+            }
+            R.id.userAgreement -> {
+            }
+            R.id.openSourceAgreement -> {
+            }
+        }
     }
 }

b84fd8319344658e053bac3a1d89a8c56ccc352d
songlan0222 <651749248@qq.com>
2020-12-23 12:57:21 +0800

    使用共享存储空间的方式，获取指定目录的访问权限

--- app/src/main/java/com/songlan/deepink/ui/local/RequestStoragePermissive.kt
+++ app/src/main/java/com/songlan/deepink/ui/local/RequestStoragePermissive.kt
@@ -1,11 +1,17 @@
 package com.songlan.deepink.ui.local
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.songlan.deepink.AppProfiles
 import com.songlan.deepink.MyApplication.Companion.context
 import com.songlan.deepink.R
 import kotlinx.android.synthetic.main.activity_request_storage_permissive.*
+
 
 class RequestStoragePermissive : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +30,10 @@ class RequestStoragePermissive : AppCompatActivity() {
 
         requestPermission.setOnClickListener {
 
-
-            finish()
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+            }
+            startActivityForResult(intent, 1)
         }
     }
 
@@ -36,5 +44,41 @@ class RequestStoragePermissive : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                1 -> {
+                    // The result data contains a URI for the document or directory
+                    data?.data?.also { uri ->
+                        takePermission(uri)
+                    }
+                }
+            }
+        }
+    }
+
+    fun queryPermission() {
+        val contentResolver = applicationContext.contentResolver
+        contentResolver.persistedUriPermissions.forEach {
+            it.uri.toString()
+        }
+    }
+
+    private fun takePermission(uri: Uri) {
+        val contentResolver = applicationContext.contentResolver
+        val takeFlags: Int =
+            Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+        contentResolver.takePersistableUriPermission(uri, takeFlags)
+        AppProfiles.saveToProfile(uri.toString(), uri)
+    }
+
+    fun releasePermission(uri: Uri) {
+        val contentResolver = applicationContext.contentResolver
+        val takeFlags: Int =
+            Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+        contentResolver.releasePersistableUriPermission(uri, takeFlags)
     }
 }

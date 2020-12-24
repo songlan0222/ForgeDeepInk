edcbc0d45650ddc2798e1fed1ded9e7822afdca9
songlan0222 <651749248@qq.com>
2020-12-24 16:59:24 +0800

    添加： 来自google的文本编码的依赖： https://mvnrepository.com/artifact/com.googlecode.juniversalchardet/juniversalchardet/1.0.3

--- app/src/main/java/com/songlan/deepink/ui/local/AddLocalBookEditActivity.kt
+++ app/src/main/java/com/songlan/deepink/ui/local/AddLocalBookEditActivity.kt
@@ -4,14 +4,20 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.v
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.documentfile.provider.DocumentFile
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.songlan.deepink.AppProfiles.DOCUMENT_URI_STRING
 import com.songlan.deepink.MyApplication.Companion.context
 import com.songlan.deepink.R
-import com.songlan.deepink.utils.ChapterDivideUtil
-import kotlinx.android.synthetic.main.activity_add_local_book.*
 import kotlinx.android.synthetic.main.activity_add_local_book.toolbar
 import kotlinx.android.synthetic.main.activity_edit_local_book.*
 import java.lang.Exception
@@ -21,6 +27,7 @@ class AddLocalBookEditActivity : AppCompatActivity() {
     private val viewModel by lazy {
         ViewModelProvider(this).get(AddLocalBookEditActivityVM::class.java)
     }
+    private lateinit var adapter: MyRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,16 +50,55 @@ class AddLocalBookEditActivity : AppCompatActivity() {
         val documentName = documentFile.name!!
         val lastPointIndex = documentName.lastIndexOf(".")
         bookNameTitle.text = documentName.substring(0, lastPointIndex)
+        bookNameEditText.setText(documentName.substring(0, lastPointIndex))
+        // 作者佚名，暂时不填
+        bookAuthorEditText.setText("")
+
 
         viewModel.chapterTitlesLiveData.observe(this, Observer { result ->
             val titles = result.getOrNull()
             if (titles != null) {
                 viewModel.chapterTitles.clear()
                 viewModel.chapterTitles.addAll(titles)
+                adapter.notifyDataSetChanged()
             }
         })
 
         // 开始处理文件
         viewModel.getChapterTitlesFromTxt(documentUri)
+
+        adapter = MyRecyclerViewAdapter(viewModel.chapterTitles)
+        chapterNameRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        chapterNameRecyclerView.adapter = adapter
+    }
+
+    inner class MyRecyclerViewAdapter(private val dataList: List<String>) :
+        RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder>() {
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val chapterItem: LinearLayout = view.findViewById(R.id.chapterItem)
+            val checkItem: CheckBox = view.findViewById(R.id.checkItem)
+            val chapterName: TextView = view.findViewById(R.id.chapterName)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_add_local_book_chapter, parent, false)
+            val viewHolder = ViewHolder(view)
+            viewHolder.chapterItem.setOnClickListener {
+                val position = viewHolder.adapterPosition
+                val chapter = dataList[position]
+                viewHolder.checkItem.isChecked = !viewHolder.checkItem.isChecked
+            }
+            return viewHolder
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val chapter = dataList[position]
+            holder.chapterName.text = chapter
+            holder.checkItem.isChecked = true
+        }
+
+        override fun getItemCount() = dataList.size
     }
 }
--- app/src/main/java/com/songlan/deepink/utils/ChapterDivideUtil.kt
+++ app/src/main/java/com/songlan/deepink/utils/ChapterDivideUtil.kt
@@ -2,23 +2,20 @@ package com.songlan.deepink.utils
 
 import android.net.Uri
 import android.util.Log
-import androidx.core.content.contentValuesOf
-import androidx.documentfile.provider.DocumentFile
 import com.songlan.deepink.MyApplication.Companion.context
 import java.io.*
-import java.net.URI
 
 object ChapterDivideUtil {
 
     // 遍历小说，获取章节名称
     fun getChapterTitlesFromTxt(documentFileUri: Uri): MutableList<String> {
         val documentInputStream = context.contentResolver.openInputStream(documentFileUri)
+        val regex = Regex("^.*第([0-9]{1,5}|[一二三四五六七八九十百千万亿]{1,5})[章回节部集卷].{0,24}")
         val titleList = mutableListOf<String>()
         try {
             val reader = BufferedReader(InputStreamReader(documentInputStream))
             reader.use {
                 reader.forEachLine { lineContent ->
-                    val regex = Regex("^.*第([0-9]{1,5}|[一二三四五六七八九十百千万亿]{1,5})[章回节部集卷].{0,24}")
                     val matchTitleList = regex.findAll(lineContent)
                     matchTitleList.forEach {
                         Log.d("MainTest", it.value)
@@ -30,7 +27,6 @@ object ChapterDivideUtil {
                 val reader = BufferedReader(InputStreamReader(documentInputStream, "GB2312"))
                 reader.use {
                     reader.forEachLine { lineContent ->
-                        val regex = Regex("^.*第([0-9]{1,5}|[一二三四五六七八九十百千万亿]{1,5})[章回节部集卷].{0,24}")
                         val matchTitleList = regex.findAll(lineContent)
                         matchTitleList.forEach {
                             Log.d("MainTest", it.value)
@@ -45,5 +41,42 @@ object ChapterDivideUtil {
         return titleList
     }
 
+    fun getFileIncode(file: File): String? {
+        if (!file.exists()) {
+            System.err.println("getFileIncode: file not exists!")
+            return null
+        }
+        val buf = ByteArray(4096)
+        var fis: FileInputStream? = null
+        try {
+            fis = FileInputStream(file)
+            // (1)
+            val detector = UniversalDetector(null)
+
+            // (2)
+            var nread: Int
+            while (fis.read(buf).also { nread = it } > 0 && !detector.isDone()) {
+                detector.handleData(buf, 0, nread)
+            }
+            // (3)
+            detector.dataEnd()
+
+            // (4)
+            val encoding: String = detector.getDetectedCharset()
+            if (encoding != null) {
+                println("Detected encoding = $encoding")
+            } else {
+                println("No encoding detected.")
+            }
+
+            // (5)
+            detector.reset()
+            fis.close()
+            return encoding
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
 
 }
